3e7f8fa7fa98f6ef80155ac89e1fda1988d52f40
Shaoqi <549730265@qq.com>
2016-07-24 16:45:58 +0800

    miniMVC Start

---
+++ shaoqi.test/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shaoqi.test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string regex = textBox1.Text.Trim();
+            string result = textBox2.Text.Trim();
+
+            bool dd = Regex.IsMatch(result, regex);
+
+            label4.Text = dd.ToString();
+        }
+    }
+}
