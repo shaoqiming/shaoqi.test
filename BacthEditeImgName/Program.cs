77386d5bc6cdc92e1a89864077b0e3912b356223
shaoqi <549730265@qq.com>
2017-03-31 09:55:44 +0800

    对文件进行批量的改名字 (面对一堆文件添加后缀太痛苦了)

---
+++ BacthEditeImgName/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BacthEditeImgName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo info = new DirectoryInfo(@"E:\iamge");
+
+            var id = 0;
+            foreach (FileInfo file in info.GetFiles("*.*"))
+            {
+                
+                string xingname = @"E:\\iamge\\转换后的文件\\" + id + ".jpg";
+                id++;
+
+                if (File.Exists(xingname))
+                {
+                    File.Delete(xingname);
+                }
+
+
+                file.MoveTo(xingname);
+            }
+        }
+    }
+}
