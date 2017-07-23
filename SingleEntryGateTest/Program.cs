cdcf11991ec264375a8e821a32c09dca9c4de00f
shaoqiming <549730265@qq.com>
2017-07-23 18:16:43 +0800

    MVC中的SingleEntryGate的测试

---
+++ SingleEntryGateTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SingleEntryGateTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleEntryGate demo = new SingleEntryGate();
+
+            var frist = demo.TryEnter();
+            var seconde = demo.TryEnter();
+
+            Console.WriteLine(frist + "....." + seconde);
+            Console.ReadKey();
+        }
+    }
+
+    internal sealed class SingleEntryGate
+    {
+        private const int NotEntered = 0;
+        private const int Entered = 1;
+
+        private int _status=9;
+
+        // returns true if this is the first call to TryEnter(), false otherwise
+        public bool TryEnter()
+        {
+            int oldStatus = Interlocked.Exchange(ref _status, Entered);
+            return (oldStatus == NotEntered);
+        }
+    }
+}
